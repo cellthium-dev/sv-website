a39aaf32963e626c1a21cd88ed616838c43d2e2a
Felix Arjuna <felixarjuna.de@yahoo.com>
2026-03-22 21:10:54 +0100

    fix: scroll to bottom of the page on über mich page

--- TODO.md
+++ TODO.md
@@ -11,7 +11,7 @@
 [x] update favicon and title for the website
 [x] fix scroll spacing in the homepage
 [x] fix cal.eu embedded link
-[ ] fix scroll to bottom on the page
+[x] fix scroll to bottom on the page
 [ ] clean up style with shadcn/ui
 use frontend + shadcn ui skills for the agent
 [ ] use themes and and dark mode
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,16 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet, useLocation } from "@tanstack/react-router";
+import React from "react";
+
+function RootComponent() {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return <Outlet />;
+}
 
 export const Route = createRootRoute({
-  component: () => <Outlet />,
+  component: RootComponent,
 });
